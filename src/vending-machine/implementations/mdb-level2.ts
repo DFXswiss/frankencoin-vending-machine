1f7c5cdddebc653826f66760afeadf9af18ebdac
nemch <emch.nicolas@bluewin.ch>
2024-09-19 15:32:23 +0200

    Update mdb-level2.ts
    Fehlerbehebung bei Errormeldungen

--- src/vending-machine/implementations/mdb-level2.ts
+++ src/vending-machine/implementations/mdb-level2.ts
@@ -47,7 +47,7 @@ export class MdbLevel2 implements VendingMachine {
   // --- PRIVATE API --- //
 
   private async onEnable() {
-    await Util.sleep(2);
+    await Util.sleep(1);
     await this.setCredit(Config.pos.credit);
   }
 
@@ -82,10 +82,10 @@ export class MdbLevel2 implements VendingMachine {
       case 'ERR': {
         if (payload[0].includes('cashless is on')) {
           await this.onEnable();
-        } else if (payload[0].includes('START') || payload[0].includes('VEND 5')) {
-          await this.disable(); // restart
-        } else if (payload[0].includes('VEND 1') || payload[0].includes('VEND 3')) {
+        } else if (payload[0].includes('VEND 1') || payload[0].includes('VEND 3') || payload[0].includes('VEND 5')) {
           await this.onEnable();
+        } else if (payload[0].includes('START -3')) {
+          await this.disable();
         } else {
           this.$message.next({ type: MessageType.ERROR, payload: payload.join(',') });
         }
